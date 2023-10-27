588824a85e9aa26f7f1e866acd890414b4c2cfcc
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-10-27 13:06:26 -0300

    Lombok atualizado

--- src/main/java/uservices/domain/Entity/Alarm.java
+++ src/main/java/uservices/domain/Entity/Alarm.java
@@ -7,7 +7,9 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
 public class Alarm {
     @Id
@@ -22,81 +24,5 @@ public class Alarm {
     private String escalation;
     private String squad;
 
-    public Alarm(Integer id, String alarm_name, String criticality, String description, String responsible, String actuation, String escalation, String squad) {
-        this.id = id;
-        this.alarm_name = alarm_name;
-        this.criticality = criticality;
-        this.description = description;
-        this.responsible = responsible;
-        this.actuation = actuation;
-        this.escalation = escalation;
-        this.squad = squad;
-    }
 
-    public Alarm() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getAlarm_name() {
-        return alarm_name;
-    }
-
-    public void setAlarm_name(String alarm_name) {
-        this.alarm_name = alarm_name;
-    }
-
-    public String getCriticality() {
-        return criticality;
-    }
-
-    public void setCriticality(String criticality) {
-        this.criticality = criticality;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getResponsible() {
-        return responsible;
-    }
-
-    public void setResponsible(String responsible) {
-        this.responsible = responsible;
-    }
-
-    public String getActuation() {
-        return actuation;
-    }
-
-    public void setActuation(String actuation) {
-        this.actuation = actuation;
-    }
-
-    public String getEscalation() {
-        return escalation;
-    }
-
-    public void setEscalation(String escalation) {
-        this.escalation = escalation;
-    }
-
-    public String getSquad() {
-        return squad;
-    }
-
-    public void setSquad(String squad) {
-        this.squad = squad;
-    }
 }
--- src/main/java/uservices/domain/Entity/Client.java
+++ src/main/java/uservices/domain/Entity/Client.java
@@ -8,7 +8,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
 public class Client {
     @Id
@@ -19,36 +21,5 @@ public class Client {
 
     private String senha;
 
-    public Client() {
-    }
 
-    public Client(Integer id, String email, String senha) {
-        this.id = id;
-        this.email = email;
-        this.senha = senha;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 }
