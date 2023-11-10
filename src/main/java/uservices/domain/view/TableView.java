8beadcd4ad55e701f70584417645dde928ee1e05
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-11-10 16:18:48 -0300

    Versão estável (.Jar nao funciona)

--- src/main/java/uservices/domain/view/TableView.java
+++ src/main/java/uservices/domain/view/TableView.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class TableView {
     @Bean
-    @GetMapping("/table")
+    @GetMapping
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("assets/table.html");
